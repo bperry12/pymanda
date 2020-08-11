53cfbbdebc36ef91b9a640ebbaa92b8a4a8a3d0a
Suhail Thahir <suhail.thahir@gmail.com>
2020-08-11 14:55:20 -0400

    Add Documentations

--- pymanda/choices.py
+++ pymanda/choices.py
@@ -1,7 +1,48 @@
 import pandas as pd
 import numpy as np
 
+"""
+ChoiceData
+---------
+A container for a DataFrame that maintains relevant columns for mergers and
+acquisitions analyses
+
+"""
 class ChoiceData():
+    """
+     Two-dimensional, size-mutable, potentially heterogeneous tabular data.
+    
+     Data structure also contains labeled axes (rows and columns).
+     Arithmetic operations align on both row and column labels. Can be
+     thought of as a dict-like container for Series objects. The primary
+     pandas data structure.
+     
+     Additionally, has parameters to identify variables of interest for 
+     calculating competition metrics using customer level data
+    
+     Parameters
+     ----------
+     data : Non-Empty pandas.core.frame.DataFrame object
+    
+     choice_var : String of Column Name
+         Column name that identifies the "choice" of each customer  
+     corp_var : String of Column Name, default None
+         Column Name that identifies a higher level classification of Choice    
+     geog_var : String of Column Name, default None
+         Column name that identifies a geography for Customer
+     wght_var : String of Column Name, default None
+         Column name that identifies weight for customer level
+    
+     Examples
+     --------
+     Constructing ChoiceData from a DataFrame.
+     
+     >>> choices = ['a' for x in range(100)]
+     
+     >>> data = pandas.DataFrame({'choice': choices})
+     >>> cd = pymanda.ChoiceData(data, 'choice')
+     """
+    
     def __init__(
         self,
         data, 
@@ -41,6 +82,43 @@ class ChoiceData():
         
         
     def estimate_psa(self, centers, threshold=[.75, .9]):
+        """
+        Return a dictionary idenftifying geographies in a choice's Primary 
+        Service Area (PSA) at a given threshold.
+
+        Dictionary keys are labeled as "{center}_{threshold}" and values are 
+        a list of geographies falling within the threshold
+        
+        Default will use self.corp_var as choice and self.wght_var for count
+        
+        Parameters
+        ----------
+        centers: List of choices to find 
+            
+        threshold : float or list of floats, default [.75, .9]
+            Threshold or levels of thresholds to find the PSA for each choice 
+            in centers
+
+        Returns
+        -------
+        Dictionary
+        
+        Examples
+        --------
+        >>> choices = ['a' for x in range(100)]
+        
+        >>> zips = [1 for x in range(20)]
+        >>> zips += [2 for x in range(20)]
+        >>> zips += [3 for x in range(20)]
+        >>> zips += [4 for x in range(20)]
+        >>> zips += [5 for x in range(20)]
+        
+        >>> data = pd.DataFrame({'choice': choices, 'geography': zips})
+        >>> cd = ChoiceData(data, 'choice', geog_var = 'geography')
+        >>> cd.estimate_psa(['a'])
+        {'a_0.75': [1, 2, 3, 4], 'a_0.9': [1, 2, 3, 4, 5]}
+        
+        """
         if self.geog_var is None:
             raise KeyError ("geog_var is not defined")
         
@@ -90,6 +168,51 @@ class ChoiceData():
         return output_dict
     
     def restrict_data(self, restriction):
+        """
+        Restrict the data is self.data using an inline series
+        
+        Parameters
+        ----------
+        restriction: pandas.core.series.Series with Boolean Date Type
+            Boolean series identifying which rows to keep given a restriction
+        
+        Examples
+        --------
+        >>> choices = ['a' for x in range(10)]
+        
+        >>> zips = [1 for x in range(2)]
+        >>> zips += [2 for x in range(2)]
+        >>> zips += [3 for x in range(2)]
+        >>> zips += [4 for x in range(2)]
+        >>> zips += [5 for x in range(2)]
+        
+        >>> data = pd.DataFrame({'choice': choices, 'geography': zips})
+        >>> cd = ChoiceData(data, 'choice')
+        >>> cd.data
+          choice  geography
+        0      a          1
+        1      a          1
+        2      a          2
+        3      a          2
+        4      a          3
+        5      a          3
+        6      a          4
+        7      a          4
+        8      a          5
+        9      a          5
+        >>> cd.restrict_data(data['geography'] != 5)
+        >>> cd.data
+          choice  geography
+        0      a          1
+        1      a          1
+        2      a          2
+        3      a          2
+        4      a          3
+        5      a          3
+        6      a          4
+        7      a          4
+        
+        """
         if type(restriction) != pd.core.series.Series:
             raise TypeError ("Expected type pandas.core.series.Series. Got {}".format(type(restriction)))
         
